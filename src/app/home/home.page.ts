27bcfbebe4ea12847d02e41f9888e49b0c9ae0d4
Alberto Lopez Martinez <krng.taure@gmail.com>
2024-09-10 20:56:21 +0200

    sum

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Component, inject } from '@angular/core';
 import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, IonItem, IonInput, IonButton } from '@ionic/angular/standalone';
 import { MessageComponent } from '../message/message.component';
@@ -11,10 +12,14 @@ import { FirebaseRTBService } from '../service/firebase-rtb.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonInput, IonItem, CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, MessageComponent],
+  imports: [IonButton, FormsModule, IonInput, IonItem, CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, MessageComponent],
 })
 export class HomePage {
   public mensajes: string[] = [];
+  public from: string = '';
+  public to: string = '';
+  public message: string = '';
+
   constructor(private furebaseService: FirebaseRTBService) {
     this.mensajes = this.furebaseService.mensajes;
   }
